f3b61bff3795ccdf8e4fec7ed432c5a52cc9df0a
Wasin Thonkaew <haxpor@gmail.com>
2015-09-27 18:47:31 +0700

    Change to use UIView instead of UIImageView.
    In .xib of ViewController, changed to use UIView instead of UIImageView.

--- demo/ViewController.h
+++ demo/ViewController.h
@@ -39,9 +39,9 @@
 - (IBAction)pause:(id)sender;
 - (IBAction)start:(id)sender;
 
-@property (weak, nonatomic) IBOutlet UIImageView *animationView;
-@property (weak, nonatomic) IBOutlet UIImageView *scaledAndTransformedView;
-@property (weak, nonatomic) IBOutlet UIImageView *staticView;
+@property (weak, nonatomic) IBOutlet UIView *animationView;
+@property (weak, nonatomic) IBOutlet UIView *scaledAndTransformedView;
+@property (weak, nonatomic) IBOutlet UIView *staticView;
 
 
 @end
